1a99b7b2221c5a7d8c94bc01f80725f2e7fcc399
st-mn <stanislav.toman@gmail.com>
2023-06-24 18:14:35 +0200

    init

--- example/views.py
+++ example/views.py
@@ -4,12 +4,20 @@ from datetime import datetime
 from django.http import HttpResponse
 
 def index(request):
+    if request.method == 'POST':
+        api_key = request.POST['api_key']
+        # Use api_key...
+        
     now = datetime.now()
     html = f'''
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
+            <form method="POST">
+                <input type="text" name="api_key">
+                <button type="submit">Submit</button>
+            </form>
         </body>
     </html>
     '''
